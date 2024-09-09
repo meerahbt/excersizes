51506c8d217acb34ee32e4b0b2222f82ce25a692
meerahbt <meerahbt@gmail.com>
2024-09-09 17:25:00 +0000

    Sep 9, 2024, 9:25 PM

--- recursion/recursion03.c
+++ recursion/recursion03.c
@@ -5,17 +5,17 @@ int sum(int n);
 
 int main(void) {
 ; 
-    printf("%i\n", sum(43));
+    printf("%i\n", sum(431));
 }
 
 int sum(int n){
-    if (n % 10 == 0)
+    if (n < 10)
     {
         return n;
     }
     else
     {
-        return (n % 10) + sum(n);
+        return (n % 10) + sum(n / 10); // checking for the last digit + recursing the digits before that 
     }
 }
 
