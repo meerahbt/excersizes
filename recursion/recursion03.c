bcfbd83a03edd0dfbdcb372f88c50c861a9e1586
meerahbt <meerahbt@gmail.com>
2024-09-09 17:17:24 +0000

    Sep 9, 2024, 9:17 PM

---
+++ recursion/recursion03.c
@@ -0,0 +1,21 @@
+// Exercise: Write a recursive function to calculate the sum of digits of a positive integer.
+#include <stdio.h>
+
+int sum(int n);
+
+int main(void) {
+; 
+    printf("%i\n", sum(43));
+}
+
+int sum(int n){
+    if (n % 10 == 0)
+    {
+        return n;
+    }
+    else
+    {
+        return (n % 10) + sum(n);
+    }
+}
+
