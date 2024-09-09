c627d8ed8192a1e1ab2b6a383aab2b7b4d9402df
meerahbt <meerahbt@gmail.com>
2024-09-09 16:56:05 +0000

    Sep 9, 2024, 8:56 PM

--- recursion/recursion02.c
+++ recursion/recursion02.c
@@ -1,1 +1,23 @@
 #include <stdio.h> 
+
+int fib(int n);
+
+int main(void){
+    fib(5);
+}
+
+// fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...etc. 
+// starts with 0 
+
+// find the nth term of the sequence using recursion 
+
+int fib(int n){
+    if (n==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return n + fib(n+1);
+    }
+}
