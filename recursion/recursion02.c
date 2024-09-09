c119bd87f29218d5b1f0a3289c7456f373e9c8c5
meerahbt <meerahbt@gmail.com>
2024-09-09 16:59:03 +0000

    Sep 9, 2024, 8:59 PM

--- recursion/recursion02.c
+++ recursion/recursion02.c
@@ -3,13 +3,13 @@
 int fib(int n);
 
 int main(void){
-    fib(5);
+    printf("%i\n", fib(5));
 }
 
 // fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...etc. 
 // starts with 0 
 
-// find the nth term of the sequence using recursion 
+// find up to the nth term of the sequence using recursion 
 
 int fib(int n){
     if (n==0)
